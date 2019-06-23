2fd68bdb6126016c5e99a3f523e70bc6090ea7ed
chatanisota <newpc1271@hotmail.co.jp>
2019-06-23 18:00:48 +0900

    [fix/footer] フッターに「・」を追加修正

--- wp-content/themes/univis_new/footer.php
+++ wp-content/themes/univis_new/footer.php
@@ -1,42 +1,42 @@
 		<footer id="footer">
 			<div class="uk-container uk-container-center">
 			<div id="f_nav">
-			
+
 					<div class="f_nav f_large">
 						<h4><a href="/service">サービス</a></h4>
 						<div class="f_nav_child">
-				
+
 								<h5 class="f_nav_tms_h5">M&A Service</h5>
-								
+
 									<ul>
-										<li><a href="/service/ma">M&Aアドバイザリー​</a></li>
-										<li><a href="/service/dd">デューデリジェンス​</a></li>
-										<li><a href="/service/dd">株価算定​</a></li>									
+										<li>・ <a href="/service/ma">M&Aアドバイザリー​</a></li>
+										<li>・ <a href="/service/dd">デューデリジェンス​</a></li>
+										<li>・ <a href="/service/dd">株価算定​</a></li>
 									</ul>
-								
-								
-								
+
+
+
 								<h5 class="f_nav_tms_h5">Consulting</h5>
-								
+
 									<ul>
-										<li><a href="/service/finance">資金調達支援​</a></li>
-										<li><a href="/service/cfo">ＣＦＯ代行支援/ベンチャー企業支援​</a></li>
-										<li><a href="/service/accounting">会計コンサルティング​</a></li>
+										<li>・ <a href="/service/finance">資金調達支援​</a></li>
+										<li>・ <a href="/service/cfo">ＣＦＯ代行支援/ベンチャー企業支援​</a></li>
+										<li>・ <a href="/service/accounting">会計コンサルティング​</a></li>
 										<!--
 										<li><a href="/service/planning">計画・予算策定支援/KPI管理​</a></li>-->
-										<li><a href="/service/international">国際業務/海外進出支援（ＵＳＡ）​</a></li>
-										<li><a href="/service/invest">自己投資・経営支援​</a></li>
+										<li>・ <a href="/service/international">国際業務/海外進出支援（ＵＳＡ）​</a></li>
+										<li>・ <a href="/service/invest">自己投資・経営支援​</a></li>
 									</ul>
 								</li>
-								
+
 								<h5 class="f_nav_tms_h5">Business Process Outsourcing</h5>
-								
+
 									<ul>
-										<li><a href="/service/tax">会計・税務顧問/申告書作成・節税支援​</a></li>
-										<li><a href="/service/audit">会計監査​</a></li>
-										<li><a href="/service/subsidy">助成金・補助金申請​</a></li>
+										<li>・ <a href="/service/tax">会計・税務顧問/申告書作成・節税支援​</a></li>
+										<li>・ <a href="/service/audit">会計監査​</a></li>
+										<li>・ <a href="/service/subsidy">助成金・補助金申請​</a></li>
 									</ul>
-								
+
 
 						</div>
 					</div>
@@ -48,7 +48,7 @@
 							<li><a href="/about/vision">理念/弊社の強み​</a></li>
 							<li><a href="/about/member">メンバープロフィール​</a></li>
 							<li><a href="/about/access">アクセス</a></li>
-						</ul>			
+						</ul>
 					</div>
 					<div class="f_nav">
 						<h4><a href="/recruit">リクルート​</a></h4>
@@ -76,7 +76,7 @@
 						</ul>
 						<h4>others</h4>
 						<ul>
-							
+
 							<li><a href="/news">News</a></li>
 							<li><a href="/privacypolicy">プライバシーポリシー​</a></li>
 							<li><a href="/sitemap">サイトマップ​​</a></li>
@@ -88,7 +88,7 @@
 					<p><img class="uk-margin uk-margin-remove" src="<?php bloginfo('template_directory'); ?>/img/logo.png" width="90" height="30" alt="ユニヴィスグループ"></p>
 					<p class="copyRight">Copyright &copy;  Univis Group. All rights reserved.</p>
 
-				</div>				
+				</div>
 			</div>
 		</footer>
         <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/uikit.min.css" />
